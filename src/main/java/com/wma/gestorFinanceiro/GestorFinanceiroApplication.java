99968d545e8d6f3024c4559bb30d4aed18deaefd
welingtonguilhardi <welingtonguilhard75@gmail.com>
2025-09-19 08:59:52 -0300

    Inicio projeto

---
+++ src/main/java/com/wma/gestorFinanceiro/GestorFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.wma.gestorFinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wma/gestorFinanceiro/GestorFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wma.gestorFinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
